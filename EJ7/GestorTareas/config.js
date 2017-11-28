572942e3da3e71f251e91c4d6ecc4c920ce44bd7
Rubén Barrado González <ruben_grang_93@hotmail.com>
2017-11-28 18:54:26 +0100

    preparando el ejrcicio 7

---
+++ EJ7/GestorTareas/config.js
@@ -0,0 +1,15 @@
+//Jaime Tamames y Ruben Barrado
+
+module.exports = {
+    /* Configuración de los datos de acceso a la BD */
+    mysqlConfig: {
+        database: "tareas",
+        host: "localhost",
+        user: "root",
+        password: "awaw"
+    },
+    
+    /* Puerto de escucha */
+    port: 3000
+};
+
