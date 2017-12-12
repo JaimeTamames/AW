ec627a3fadf8dbae27588c53d2e1a7a33d3ce0c2
Rubén Barrado González <ruben_grang_93@hotmail.com>
2017-12-13 00:38:29 +0100

    SQL ACTUALIZADO

--- PR1/Enunciado y sql/facebluff.sql
+++ PR1/Enunciado y sql/facebluff.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 12-12-2017 a las 19:24:49
+-- Tiempo de generación: 13-12-2017 a las 00:38:02
 -- Versión del servidor: 10.1.28-MariaDB
 -- Versión de PHP: 5.6.32
 
@@ -39,7 +39,8 @@ CREATE TABLE `sessions` (
 --
 
 INSERT INTO `sessions` (`session_id`, `expires`, `data`) VALUES
-('JxH9ywcxNkYRLPTVl9wWOQNgp6eqtLrq', 1513166477, '{\"cookie\":{\"originalMaxAge\":null,\"expires\":null,\"httpOnly\":true,\"path\":\"/\"},\"currentUser\":\"jmc@telefonica.com\"}');
+('VMMtmpKKNwevDXjHrvCDkPvEaFkedK1j', 1513206822, '{\"cookie\":{\"originalMaxAge\":null,\"expires\":null,\"httpOnly\":true,\"path\":\"/\"},\"currentUser\":\"usuario@ucm.es\"}'),
+('ylTTBOx-6YOnGM1Wk--arUbV_dDpAODw', 1513182841, '{\"cookie\":{\"originalMaxAge\":null,\"expires\":null,\"httpOnly\":true,\"path\":\"/\"},\"currentUser\":\"ruben@r.com\"}');
 
 -- --------------------------------------------------------
 
@@ -53,7 +54,7 @@ CREATE TABLE `user` (
   `img` varchar(100) DEFAULT NULL,
   `sexo` varchar(50) DEFAULT NULL,
   `puntuacion` int(11) DEFAULT NULL,
-  `edad` int(11) DEFAULT NULL,
+  `fechaNacimiento` varchar(10) DEFAULT NULL,
   `nombre` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -61,8 +62,13 @@ CREATE TABLE `user` (
 -- Volcado de datos para la tabla `user`
 --
 
-INSERT INTO `user` (`email`, `password`, `img`, `sexo`, `puntuacion`, `edad`, `nombre`) VALUES
-('princesa@monica.com', 'monica', 'Stuart-01.png', 'Femenino', 0, 18, 'Monica Moran');
+INSERT INTO `user` (`email`, `password`, `img`, `sexo`, `puntuacion`, `fechaNacimiento`, `nombre`) VALUES
+('usuario@ucm.es', 'mipass', 'img2315.png', 'Femenino', 55, '10/11/1963', 'UCM'),
+('ruben@r.com', 'qwerty', 'Stuart-01.png', 'Masculino', 0, '0000-00-00', 'Ruben Barrado González'),
+('monica@m.com', 'qwerty', 'Dave-01.png', 'Femenino', 0, '0000-00-00', 'Monica moran'),
+('rbg@r.com', 'qwerty', 'Bat-01.png', 'Masculino', 0, '0000-00-00', 'prueba edad'),
+('rubern@ruben.com', 'qwerty', 'Harley-01.png', 'Masculino', 0, '02/12/1993', 'ruben edad'),
+('rubeern@ruben.com', 'wertyrt', 'Harley-01.png', 'Masculino', 0, '02/12/1993', 'ruben edad');
 
 --
 -- Índices para tablas volcadas
