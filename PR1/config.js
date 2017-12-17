9e7538a33e1095d80bcb07582efb82f4d012c98b
Jaime Tamames <jaimetamames@hotmail.com>
2017-12-18 00:12:53 +0100

    .

--- PR1/config.js
+++ PR1/config.js
@@ -3,10 +3,6 @@
 module.exports = {
     /* Configuración de los datos de acceso a la BD */
     mysqlConfig: {
-        database: "facebluff",
-        host: "localhost",
-        user: "root",
-        password: "awaw"
         dbName: "BDPractica1",
         dbHost: "localhost",
         dbUser: "root",
@@ -15,8 +11,6 @@ module.exports = {
 		port: 3000
     },
     
-    /* Puerto de escucha */
-    port: 3000
     
 };
 
