5b091f9cdb4cb6134cf642573d9407cd48988d16
Rubén Barrado González <ruben_grang_93@hotmail.com>
2017-12-17 23:42:37 +0100

    añadido placeholder

--- PR1/config.js
+++ PR1/config.js
@@ -3,7 +3,7 @@
 module.exports = {
     /* Configuración de los datos de acceso a la BD */
     mysqlConfig: {
-        database: "faceblufff",
+        database: "facebluff",
         host: "localhost",
         user: "root",
         password: "awaw"
