6dc5cc386c5813dfb07382556a62e3907adbf063
Rubén Barrado González <ruben_grang_93@hotmail.com>
2018-01-09 16:41:28 +0100

    plantilla patri

--- EJ8/PlantillaPatri/public/js/index.js
+++ EJ8/PlantillaPatri/public/js/index.js
@@ -31,7 +31,26 @@ function taskToDOMElement(task) {
 }
 
 function loadTasks() {
-    // Implementar
+    
+    $.ajax({
+
+        type: "GET",
+        url: "/tasks",
+
+        // En caso de éxito, colocamos el texto con el resultado
+        // en el documento HTML
+        success: function (data,textStatus, jqXHR){
+            data.forEach(element => {
+                $("#result").before(taskToDOMElement(element));
+            });
+
+        },
+        //En caso de error, mostramos el error producido
+        error: function (jqXHR, textStatus, errorThrown) {
+            alert("Se ha producido un error: " + errorThrown);
+        }
+    }); 
+        
 }
 
 function onRemoveButtonClick(event) {
@@ -44,10 +63,45 @@ function onRemoveButtonClick(event) {
     let liPadre = selected.parent();
 
     // Implementar el resto del método aquí.
-    // ...
+    $.ajax({
+        type: "Delete",
+        url: "/tasks/" + liPadre.data("id"),
+        success:function(data){
+            liPadre.remove();
+        },
+        //En caso de error, mostramos el error producido
+        error: function (jqXHR, textStatus, errorThrown) {
+            alert("Se ha producido un error: " + errorThrown);
+        }
+
+    });
 }
 
 function onAddButtonClick(event) {
-    // Implementar
+    
+    var newTag = document.getElementsByName("taskText")[0].value; //Tema 7 diap 35
+
+    if(newTag.length > 0){
+        $.ajax({ //Diapositiva 79 tema 8
+            type: "POST",
+            url:"/tasks",
+            contentType:"application/json",
+            data:JSON.stringify({text:newTag}),
+
+            success: (data,textStatus, jqXHR)=>{
+                $("#result").before(taskToDOMElement(data));
+
+            },
+            //En caso de error, mostramos el error producido
+            error: function (jqXHR, textStatus, errorThrown) {
+                alert("Se ha producido un error: " + errorThrown);
+            }
+
+        });
+    }else{
+        alert("No puedes añadir una tarea vacia.")
+
+    }
+
 }
 
